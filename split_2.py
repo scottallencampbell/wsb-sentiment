f72c7c0d0702708d8c296598d185bee775d04117
scottallemcampbell <no.spam@hammerbeam.com>
2022-11-30 16:26:44 -0600

    Fixed sync code to remove duplicate sentiments

--- split_2.py
+++ split_2.py
@@ -20,6 +20,7 @@ comments_files = os.listdir(comments_path)
 
 print(f'Loading sentiments from flat file')
 sentiments = {}
+uncalculated_sentiments = {}
 
 with open(sentiments_file, 'r') as psvfile:
 	for row in csv.reader(psvfile, dialect='piper'):
@@ -32,26 +33,26 @@ for comments_file in comments_files:
 	
 	if date in sentiments:
 		print(f'Skipping {comments_file}, this date has already been evaluated')
-
-#filename = f'{comments_path}{value}.txt'
+	else:
+		uncalculated_sentiments[date] = date
 
 thread = 0
 splitname = ''
-stride = math.ceil(len(sentiments)/threads)
+stride = math.ceil(len(uncalculated_sentiments)/threads)
 counter = 0
 
-for i in range(0, len(sentiments), stride):
+for i in range(0, len(uncalculated_sentiments), stride):
 	splitname = f'./{counter}.py'
 		
 	with open(splitname, 'w') as f:
-		f.write('from sentiment import calculate_aggregate_sentiment\n')
+		f.write('from sentiments import calculate_aggregate_sentiment\n')
 
 	for j in range(i, i + stride - 1):
 		
-		if j >= len(sentiments):
+		if j >= len(uncalculated_sentiments):
 			continue
 		
-		value = list(sentiments.items())[j]
+		value = list(uncalculated_sentiments.items())[j]
 		
 		with open(splitname, 'a') as f:
 			f.write(f'calculate_aggregate_sentiment(\'./data/comments/{value[0]}.txt\')\n')
