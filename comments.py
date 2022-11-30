50db5f28c7549f1100b1d76c8b72983b8ec43222
scottallemcampbell <no.spam@hammerbeam.com>
2022-11-30 11:04:50 -0600

    Continued calculating sentiments

--- comments.py
+++ comments.py
@@ -2,7 +2,7 @@ import json
 import os
 import pandas as pd
 import time
-from datetime import datetime, timedelta
+import datetime
 from posts import get_existing_posts
 from api import redditApi
 
@@ -17,33 +17,34 @@ def download_comments():
         download_comments_by_post(key, posts[key], filename)
         
 def download_comments_by_post(post_id, date, filename):
-    if os.path.exists(filename):
-        #print(f'Skipping {date}.txt, file already exists')
-        True
-    else:
-        print(f'Downloading comments for {date}')
-        start = time.time()
-        comments = []
-        submission = redditApi.submission(post_id)
+    start = datetime.datetime.now().date()
+    
+    if start >= datetime.datetime.strptime(date, '%Y-%m-%d').date():
+        return
+    
+    print(f'Downloading comments for {date}')
+    
+    comments = []
+    submission = redditApi.submission(post_id)
 
-        submission.comments.replace_more(limit=None)
+    submission.comments.replace_more(limit=None)
 
-        # only gettting top-level comments for now
-        for comment in submission.comments:
-            comments.append([
-                comment.id[-6:], 
-                comment.link_id[-6:], 
-                comment.parent_id[-6:], 
-                str(comment.score), 
-                comment.body.replace('|', '').replace('\r', ' ').replace('\n', ' '),
-                datetime.fromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-            ])
-                
-        with open(filename, 'w', encoding='utf-8') as f:
-            for comment in comments:
-                f.write('|'.join(comment) + '\n')
+    # only gettting top-level comments for now
+    for comment in submission.comments:
+        comments.append([
+            comment.id[-6:], 
+            comment.link_id[-6:], 
+            comment.parent_id[-6:], 
+            str(comment.score), 
+            comment.body.replace('|', '').replace('\r', ' ').replace('\n', ' '),
+            datetime.fromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S')
+        ])
+            
+    with open(filename, 'w', encoding='utf-8') as f:
+        for comment in comments:
+            f.write('|'.join(comment) + '\n')
 
-        end = time.time()
-        print(f'Downloaded {len(submission.comments)} comments for {date} in {int(end - start)} seconds')
+    end = time.time()
+    print(f'Downloaded {len(submission.comments)} comments for {date} in {int(end - start)} seconds')
 
 
--- synchronize.py
+++ synchronize.py
@@ -1,11 +1,10 @@
 from posts import download_posts
 from comments import download_comments
 from comments import download_comments_by_post
-from sentiment import update_sentiments
+from sentiments import update_sentiments
 
-#posts = download_posts()
-#comments = download_comments()
-
+posts = download_posts()
+comments = download_comments()
 update_sentiments()
 
 # download_comments_by_post('yi6wh1', '2022-10-31', 'data/comments/2022-10-31.txt')
