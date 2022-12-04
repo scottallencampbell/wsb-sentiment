e86f1d2d4578b154a123b8c305dd6af181cf074d
Scott Campbell <scampbell@lifestart.net>
2022-12-04 13:59:59 -0600

    Updated qqq and sp500 data

--- regression.py
+++ regression.py
@@ -14,24 +14,54 @@ from sklearn.linear_model import LinearRegression
 
 sentiments_filename = './data/sentiments.txt'
 prices_filename = './data/sp500.txt'
+maximum_days_delayed = 3
 
-prices = pd.read_csv(prices_filename, sep='|', index_col=False)
-prices['date'] = pd.to_datetime(prices['date'], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
+def plot(df):
+
+    fig, ax = plt.subplots()
+    ax.plot(df['date_date'], df['close'], color='green')
+    ax.tick_params(axis='y', labelcolor='green')
+
+    ax2 = ax.twinx()
+
+    ax2.plot(df['date_date'], df['sentiment_mean'], color='red')
+    ax2.tick_params(axis='y', labelcolor='red')
+
+prices = pd.read_csv(prices_filename, sep='|', index_col=False).sort_values('date')
 prices['change'] = prices['close'] - prices['open']
 prices['change%'] = 100 * prices['change'] / prices['open']
+prices['date'] = pd.to_datetime(prices['date'])
 
-sentiments = pd.read_csv(sentiments_filename, sep='|', index_col=False)
+sentiments = pd.read_csv(sentiments_filename, sep='|', index_col=False).sort_values('date')
+sentiments['date'] = pd.to_datetime(sentiments['date'])
+sentiments.insert(1, 'next_market_date', sentiments['date'])
 
-df = pd.merge(prices, sentiments, on='date')
+# I'm sure there's a much more elegant way to join sentiments to the next day's market movement
+# sentiments['next_market_date'] = prices[prices.date > sentiments['date'].values].iloc[0]['date']
 
-#plt.figure()
-#sns.pairplot(df[['change%', 'sentiment_mean', 'sentiment_std', 'impact_mean', 'impact_std']],  markers=["o", "s"])
-#plt.show()
+for i in range(len(sentiments)):
+    date = sentiments.loc[i, 'date']
+    next_market_date = prices[prices.date > date].iloc[0]['date']
 
-X = df[['count', 'sentiment_mean', 'sentiment_std', 'impact_mean', 'impact_std']]
+    if next_market_date is not None:
+        sentiments.loc[i, 'next_market_date'] = next_market_date
+
+sentiments.rename(columns={'date': 'sentiment_date'}, inplace=True)
+
+delayed = sentiments[(sentiments['next_market_date'] - sentiments['sentiment_date']).dt.days > maximum_days_delayed ].index
+sentiments.drop(delayed, inplace=True)
+
+df = pd.merge(prices, sentiments, how='inner', left_on='date', right_on='next_market_date')
+
+#plot(df)
+#X = df[['count', 'sentiment_mean', 'sentiment_std', 'impact_mean', 'impact_std']]
+#X = df[['impact_mean', 'impact_std']]
+X = df[['sentiment_mean', 'sentiment_std']]
 y = df['change%']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 lr = LinearRegression()
 lr.fit(X_train, y_train)
+lr.score(X_test, y_test)
+
